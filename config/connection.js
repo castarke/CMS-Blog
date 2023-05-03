8e7a655df612cc057d9a69cd2de460dd5049a3ab
castarke <candler.starke@yahoo.com>
2023-05-03 01:06:26 -0400

    debugging heroku upload

--- config/connection.js
+++ config/connection.js
@@ -4,16 +4,7 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(
-    process.env.JAWSDB_NAME,
-    process.env.JAWSDB_USER,
-    process.env.JAWSDB_PASSWORD,
-    {
-      host:process.env.JAWSDB_HOST,
-      dialect:'mysql',
-      port:process.env.JAWSDB_PORT
-    }
-);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
